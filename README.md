64dc41ba9209b2779f4cf77f8ccd3bacb178dee3
rupesh sahu <rupeshsahu7541832256@gmail.com>
2022-03-28 23:10:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# web-development-fulll-apni-kasha
---
+++ and or operators15.js
@@ -0,0 +1,30 @@
+// and or operator
+
+let firstName ="rupesh";
+let age = 22;
+
+// if(firstName[0] ==="H") {
+//     console.log("your name starts with H")
+// }
+
+// if(age>18){
+//     console.log("you are above 18");
+
+// }
+
+
+// if(firstName[0] == "H" && age>18){
+//     console.log("Name starts with H and above 18");
+// } else{
+//     console.log("inside else");
+// }
+
+let firstName = "rupesh";
+let age= 19;
+
+
+if(firstName[0] === "H" || age>18){
+    console.log("Name starts wityh H abd aboive 18");
+}else{
+    console.log("inside else");
+}
